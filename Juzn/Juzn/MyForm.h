5c443a5def6c6d1d3143a2ff0ac0ceb6729a80c9
= <=>
2017-04-23 21:51:56 +0300

    game life

--- Juzn/Juzn/MyForm.h
+++ Juzn/Juzn/MyForm.h
@@ -16,11 +16,28 @@ namespace Juzn {
 	{
 	private: System::Windows::Forms::Timer^  timer1;
 	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::RadioButton^  Check_Glider;
+	private: System::Windows::Forms::RadioButton^  Check_Dot;
+	private: System::Windows::Forms::RadioButton^  Check_Rifle;
+	private: System::Windows::Forms::CheckBox^  checkBox1;
+
+
+
+
+
+
+
+
+
+
 			 Graphics ^ g;
 	public:
 		MyForm(void)
 		{
-			InitializeComponent();
+			InitializeComponent();			
 			GenerateRandom();
 
 			//g = pictureBox1->CreateGraphics();
@@ -81,6 +98,13 @@ namespace Juzn {
 			this->trackBar1 = (gcnew System::Windows::Forms::TrackBar());
 			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->Check_Glider = (gcnew System::Windows::Forms::RadioButton());
+			this->Check_Dot = (gcnew System::Windows::Forms::RadioButton());
+			this->Check_Rifle = (gcnew System::Windows::Forms::RadioButton());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->BeginInit();
 			this->tableLayoutPanel1->SuspendLayout();
@@ -95,7 +119,7 @@ namespace Juzn {
 			// pictureBox1
 			// 
 			this->pictureBox1->BackColor = System::Drawing::SystemColors::Control;
-			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->pictureBox1->Cursor = System::Windows::Forms::Cursors::Default;
 			this->pictureBox1->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->pictureBox1->Location = System::Drawing::Point(0, 0);
@@ -104,6 +128,7 @@ namespace Juzn {
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
 			this->pictureBox1->TabIndex = 0;
 			this->pictureBox1->TabStop = false;
+			this->pictureBox1->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::pictureBox1_MouseClick);
 			// 
 			// label1
 			// 
@@ -111,7 +136,7 @@ namespace Juzn {
 			this->label1->Location = System::Drawing::Point(3, 5);
 			this->label1->Margin = System::Windows::Forms::Padding(3, 5, 3, 5);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(55, 21);
+			this->label1->Size = System::Drawing::Size(55, 19);
 			this->label1->TabIndex = 2;
 			this->label1->Text = L"Скорость";
 			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
@@ -122,11 +147,11 @@ namespace Juzn {
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
 			this->trackBar1->LargeChange = 2;
-			this->trackBar1->Location = System::Drawing::Point(66, 5);
+			this->trackBar1->Location = System::Drawing::Point(71, 5);
 			this->trackBar1->Margin = System::Windows::Forms::Padding(3, 5, 3, 5);
 			this->trackBar1->Name = L"trackBar1";
 			this->trackBar1->RightToLeft = System::Windows::Forms::RightToLeft::Yes;
-			this->trackBar1->Size = System::Drawing::Size(107, 21);
+			this->trackBar1->Size = System::Drawing::Size(102, 19);
 			this->trackBar1->TabIndex = 1;
 			this->trackBar1->Scroll += gcnew System::EventHandler(this, &MyForm::trackBar1_Scroll);
 			// 
@@ -134,18 +159,27 @@ namespace Juzn {
 			// 
 			this->tableLayoutPanel1->ColumnCount = 2;
 			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
-				35.79546F)));
+				39.20454F)));
 			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
-				64.20454F)));
+				60.79546F)));
 			this->tableLayoutPanel1->Controls->Add(this->trackBar1, 1, 0);
 			this->tableLayoutPanel1->Controls->Add(this->label1, 0, 0);
 			this->tableLayoutPanel1->Controls->Add(this->button1, 1, 1);
+			this->tableLayoutPanel1->Controls->Add(this->button2, 0, 1);
+			this->tableLayoutPanel1->Controls->Add(this->button3, 0, 2);
+			this->tableLayoutPanel1->Controls->Add(this->Check_Glider, 1, 3);
+			this->tableLayoutPanel1->Controls->Add(this->Check_Dot, 0, 3);
+			this->tableLayoutPanel1->Controls->Add(this->Check_Rifle, 1, 4);
+			this->tableLayoutPanel1->Controls->Add(this->checkBox1, 1, 2);
 			this->tableLayoutPanel1->Location = System::Drawing::Point(12, 12);
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
-			this->tableLayoutPanel1->RowCount = 2;
+			this->tableLayoutPanel1->RowCount = 5;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 30)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(176, 61);
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 27)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 23)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 23)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(176, 132);
 			this->tableLayoutPanel1->TabIndex = 3;
 			this->tableLayoutPanel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::tableLayoutPanel1_Paint);
 			// 
@@ -154,19 +188,93 @@ namespace Juzn {
 			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->button1->Location = System::Drawing::Point(66, 34);
+			this->button1->Location = System::Drawing::Point(71, 32);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(107, 24);
+			this->button1->Size = System::Drawing::Size(102, 24);
 			this->button1->TabIndex = 3;
 			this->button1->Text = L"Перезапуск";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(3, 32);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(59, 22);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"Пауза";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(3, 62);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(62, 21);
+			this->button3->TabIndex = 5;
+			this->button3->Text = L"Очистить";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// Check_Glider
+			// 
+			this->Check_Glider->AutoSize = true;
+			this->Check_Glider->Location = System::Drawing::Point(71, 89);
+			this->Check_Glider->Name = L"Check_Glider";
+			this->Check_Glider->Size = System::Drawing::Size(67, 17);
+			this->Check_Glider->TabIndex = 6;
+			this->Check_Glider->Text = L"Глайдер";
+			this->Check_Glider->UseVisualStyleBackColor = true;
+			// 
+			// Check_Dot
+			// 
+			this->Check_Dot->AutoSize = true;
+			this->Check_Dot->Checked = true;
+			this->Check_Dot->Location = System::Drawing::Point(3, 89);
+			this->Check_Dot->Name = L"Check_Dot";
+			this->Check_Dot->Size = System::Drawing::Size(55, 17);
+			this->Check_Dot->TabIndex = 7;
+			this->Check_Dot->TabStop = true;
+			this->Check_Dot->Text = L"Точка";
+			this->Check_Dot->UseVisualStyleBackColor = true;
+			// 
+			// Check_Rifle
+			// 
+			this->Check_Rifle->AutoSize = true;
+			this->Check_Rifle->Location = System::Drawing::Point(71, 112);
+			this->Check_Rifle->Name = L"Check_Rifle";
+			this->Check_Rifle->Size = System::Drawing::Size(57, 17);
+			this->Check_Rifle->TabIndex = 8;
+			this->Check_Rifle->Text = L"Ружьё";
+			this->Check_Rifle->UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(194, 12);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(10, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L" ";
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Checked = true;
+			this->checkBox1->CheckState = System::Windows::Forms::CheckState::Checked;
+			this->checkBox1->Location = System::Drawing::Point(71, 62);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(92, 17);
+			this->checkBox1->TabIndex = 9;
+			this->checkBox1->Text = L"Замкнутость";
+			this->checkBox1->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(619, 511);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->tableLayoutPanel1);
 			this->Controls->Add(this->pictureBox1);
 			this->Name = L"MyForm";
@@ -176,39 +284,51 @@ namespace Juzn {
 			this->tableLayoutPanel1->ResumeLayout(false);
 			this->tableLayoutPanel1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 
 		Bitmap^ curr, ^next;
 		int w = 100, h = 100, scale = 2;
+		int red = Color::Red.ToArgb();
 
 		bool GetPixel(int x, int y)
 		{
-			if (x < 0)
-				x = w + x;
-			if (x >= w)
-				x = x - w;
+			if (checkBox1->Checked)
+			{
+				if (x < 0)
+					x = w + x;
+				if (x >= w)
+					x = x - w;
 
-			if (y < 0)
-				y = h + y;
-			if (y >= h)
-				y = y - h;
+				if (y < 0)
+					y = h + y;
+				if (y >= h)
+					y = y - h;
+			}
+			else if (x < 0 || x >= w || y < 0 || y >= h)
+				return false;
 
-			return (curr->GetPixel(x*scale, y*scale).ToArgb() == Color::Red.ToArgb());
+			return (curr->GetPixel(x*scale, y*scale).ToArgb() == red);
 		}
 
 		void SetPixel(int x, int y, bool live)
 		{
-			if (x < 0)
-				x = w + x;
-			if (x >= w)
-				x = x - w;
+			if (checkBox1->Checked)
+			{
+				if (x < 0)
+					x = w + x;
+				if (x >= w)
+					x = x - w;
 
-			if (y < 0)
-				y = h + y;
-			if (y >= h)
-				y = y - h;
+				if (y < 0)
+					y = h + y;
+				if (y >= h)
+					y = y - h;
+			}
+			else if (x < 0 || x >= w || y < 0 || y >= h)
+				return;
 
 			for (int i = 0; i < scale-1; i++)
 				for (int j = 0; j < scale-1; j++)
@@ -223,7 +343,7 @@ namespace Juzn {
 
 			for (int i = 0; i < w; i++)
 				for (int j = 0; j < h; j++)
-				SetPixel(i, j, r->NextDouble() < .3f);
+				SetPixel(i, j, r->NextDouble() < .5f);
 			
 			/*for (int i = 0; i < w; i++)
 				for (int j = 0; j < h; j++)
@@ -252,6 +372,7 @@ namespace Juzn {
 						neighbors += (int)GetPixel(x + i, y + j);
 			if (GetPixel(x, y))
 				return neighbors > 1 && neighbors < 4;
+				//return neighbors == 3 || neighbors == 5;
 			else
 				return neighbors == 3;
 		}
@@ -281,7 +402,6 @@ namespace Juzn {
 		curr = next;
 		next = s;
 
-
 		pictureBox1->BackgroundImage = curr;
 		pictureBox1->Invalidate();
 
@@ -293,6 +413,102 @@ private: System::Void tableLayoutPanel1_Paint(System::Object^  sender, System::W
 }
 private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 	GenerateRandom();
+}
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	if (timer1->Enabled)
+	{
+		button2->Text = "Пуск";
+		timer1->Enabled = false;
+	}
+	else
+	{
+		button2->Text = "Пауза";
+		timer1->Enabled = true;
+	}
+}
+
+public: Point GetMousePositionWindowsForms()
+{
+	System::Drawing::Point point = Control::MousePosition;
+	return Point(point.X, point.Y);
+}
+		/*
+public static void Copy(Bitmap target, Bitmap source, int x, int y)
+{
+	Graphics g = Graphics::FromImage(target);
+	g.DrawImage(source, x, y);
+}*/
+
+private: System::Void pictureBox1_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+	int x = (double)(e->Location.X+5) / ((double)pictureBox1->Width / (double)w), y = (double)(e->Location.Y+5) / ((double)pictureBox1->Height / (double)h);
+	
+	Bitmap ^s = curr;
+	curr = next;
+	next = s;
+	//next = curr->Clone(curr->GetBounds(System::Drawing:), curr->PixelFormat));
+
+	//label2->Text = (!GetPixel(x, y)).ToString();
+
+	if (Check_Dot->Checked)
+		SetPixel(x, y, e->Button == System::Windows::Forms::MouseButtons::Left);
+	else if (Check_Glider->Checked)
+	{
+		SetPixel(0 + x, 0 + y, true);
+		SetPixel(1 + x, 1 + y, true);
+		SetPixel(2 + x, 1 + y, true);
+		SetPixel(0 + x, 2 + y, true);
+		SetPixel(1 + x, 2 + y, true);
+	}
+	else if (Check_Rifle->Checked)
+	{
+		bool b[9][36] = {
+			{0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+			{0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+			{1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,},
+			{1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,},
+			{0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,},
+			{0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,},
+			{0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,}, };
+
+		for (int i = 0; i < 9; i++)
+			for (int j = 0; j < 36; j++)
+				SetPixel(x + j, y + i, b[i][j]);
+	
+	}
+	Bitmap ^c = curr;
+	curr = next;
+	next = c;
+
+	pictureBox1->BackgroundImage = curr;
+	pictureBox1->Invalidate();
+}
+
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+
+	Random ^r = gcnew Random();
+	curr = gcnew Bitmap(w*scale, h*scale);
+	next = gcnew Bitmap(w*scale, h*scale);
+
+	for (int i = 0; i < w; i++)
+		for (int j = 0; j < h; j++)
+			SetPixel(i, j, false);
+
+	int a = w / 2, b = h / 2;
+	SetPixel(1 + a, 1 + b, true);
+	SetPixel(2 + a, 2 + b, true);
+	SetPixel(3 + a, 2 + b, true);
+	SetPixel(1 + a, 3 + b, true);
+	SetPixel(2 + a, 3 + b, true);
+
+
+	Bitmap ^s = curr;
+	curr = next;
+	next = s;
+
+	pictureBox1->BackgroundImage = curr;
+
 }
 };
 }
